043f7ba88385a2f97463c17c96879986c29181de
Egor <Jeglederefter@yandex.ru>
2023-07-07 16:11:02 +0300

    feat: unused imports were removed

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,9 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.User;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class FilmorateApplication {
@@ -12,12 +9,6 @@ public class FilmorateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 
-
-
-
-
-//		User user = new User(1, "user@mail.ru", "name", "name",
-//				LocalDate.of(1299, 12, 12));
 	}
 
 }
