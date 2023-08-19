74f1a8dd93c0cfae82ac1a8ee6c5603d48da0989
Egor <Jeglederefter@yandex.ru>
2023-08-19 23:38:12 +0300

    feat: unused imports removed

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmValidator.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
